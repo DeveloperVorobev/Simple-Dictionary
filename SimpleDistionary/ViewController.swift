4fb13592a95e3c65d9f771bd2eece93d5a8a48ee
Vladislav Vorobev <vlad_imbo1994@mail.ru>
2022-05-18 20:49:55 +0500

    Initial Commit

---
+++ SimpleDistionary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleDistionary
+//
+//  Created by Владислав Воробьев on 18.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
